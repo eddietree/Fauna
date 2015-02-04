4fbc9a93d3b1035e1e8a305f9f4529fed0e5a54e
eddietree <eddie@illogictree.com>
2015-02-04 00:48:24 -0800

    rotation input

--- Unity/Assets/Scripts/Polyball.cs
+++ Unity/Assets/Scripts/Polyball.cs
@@ -8,6 +8,8 @@ public class Polyball : MonoBehaviour {
 	private int[] vertsIndices;
 	private Color32[] vertsColor;
 
+	private Vector2 mousePosPrev;
+
 	void CreateMesh() {
 		Mesh mesh = new Mesh ();
 		GetComponent<MeshFilter> ().mesh = mesh;
@@ -56,16 +58,69 @@ public class Polyball : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		mousePosPrev = new Vector2 (0.0f, 0.0f);
 		CreateMesh ();
 	}
+
+	void OnTouchStart( Vector2 aPos ) {
+	}
+
+	void OnTouchEnd( Vector2 aPos, Vector2 aRelativePos ) {
+	}
+
+	void OnTouchMove( Vector2 aPos, Vector2 aRelativePos ) {
 	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			transform.Rotate (10.0f, 0.0f, 0.0f);
+		Vector3 force = Vector3.right * aRelativePos.y - Vector3.up * aRelativePos.x;
+		
+		//rigidbody.rotation
+		float speed = 200.0f;
+		rigidbody.AddTorque( force * speed );
+	}
+
+	void HandleRotationInput()
+	{
+		//if (Input.GetMouseButton (0)) {
+		if (Input.touchCount > 0) {
+			Touch touch = Input.GetTouch (0);
+			
+			if (touch.phase == TouchPhase.Began) {
+				OnTouchStart (touch.position);
+
+			} else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary) {
+				OnTouchMove (touch.position, touch.deltaPosition);
+				
+			} else if (touch.phase == TouchPhase.Ended) {
+				OnTouchEnd (touch.position, touch.deltaPosition);
+			}
+		} else {
+
+			Vector3 inputMousePos = Input.mousePosition;
+			Vector2 mousePosCurr = new Vector2( inputMousePos.x, inputMousePos.y );
+			Vector2 mousePosRel = mousePosCurr - mousePosPrev;
+
+			if ( Input.GetMouseButtonDown(0) )
+			{
+				OnTouchStart ( mousePosCurr );
+
+			}
+			else if ( Input.GetMouseButtonUp(0) )
+			{
+				OnTouchEnd( mousePosCurr, mousePosRel );
+			}
+			else if ( Input.GetMouseButton(0) )
+			{
+				OnTouchMove( mousePosCurr, mousePosRel );
+			}
+
+			mousePosPrev = mousePosCurr;
 		}
+		
+		//transform.Rotate (1.0f, 0.0f, 0.0f);
+	}
 
-
-		transform.Rotate (1.0f, 0.0f, 0.0f);
+	// Update is called once per frame
+	void FixedUpdate () {
+		HandleRotationInput ();
 	}
 }
