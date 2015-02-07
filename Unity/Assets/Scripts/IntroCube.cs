cd4f31f898e497a78c1b59f4e3cf19c3f6f05b6c
eddietree <eddie@illogictree.com>
2015-02-07 01:47:06 -0800

    added toms sound

--- Unity/Assets/Scripts/IntroCube.cs
+++ Unity/Assets/Scripts/IntroCube.cs
@@ -15,7 +15,7 @@ public class IntroCube : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//transform.Rotate( 0.0f, Time.deltaTime * 30.0f, 0.0f );
 	}
 
 	void OnMouseDown() {
@@ -25,6 +25,7 @@ public class IntroCube : MonoBehaviour {
 			iTween.MoveTo (gameObject, new Vector3 (0.0f, 5.0f, 0.0f), 1.5f);
 
 			katamari.SetActive(true);
+			audio.Play ();
 
 			iTween.PunchScale( katamari, Vector3.one*0.5f, 1.5f );
 		}
