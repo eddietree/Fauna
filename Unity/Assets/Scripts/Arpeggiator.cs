3a079ec513b50f30c2f88ab5232976e6c8659bf6
eddietree <eddie@illogictree.com>
2015-02-07 00:28:08 -0800

    changed light color

--- Unity/Assets/Scripts/Arpeggiator.cs
+++ Unity/Assets/Scripts/Arpeggiator.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Arpeggiator : MonoBehaviour {
 
 	public Camera camera;
+	public Light light;
 	public float nightCoeff;
 	public InputRotator inputRotator;
 
@@ -14,10 +15,17 @@ public class Arpeggiator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		// bg color
 		Color colorDay = new Color( 243.0f/255.0f, 233.0f/255.0f, 93.0f/255.0f );
 		Color colorNight = new Color( 81.0f/255.0f, 85.0f/255.0f, 141.0f/255.0f );
-
 		camera.backgroundColor = Color.Lerp (colorDay, colorNight, nightCoeff);
+
+		// light color
+		Color lightColorDay = new Color( 255.0f/255.0f, 244.0f/255.0f, 221.0f/255.0f );
+		Color lightColorNight = new Color( 140.0f/255.0f, 143.0f/255.0f, 180.0f/255.0f );
+		light.color = Color.Lerp (lightColorDay, lightColorNight, nightCoeff);
+
 	
 	}
 }
